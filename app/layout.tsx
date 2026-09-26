de4ddcfe4defb267683dda97c148c842c197d9a8
linuskatzenbach <katzenbachlinus@gmail.com>
2026-09-26 12:33:02 +0200

    Update metadata title and description for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Billard Elo",
-  description: "Elo-Rangliste für unsere Billard-Runde",
+  title: "Billard-Rangliste",
+  description: "Jetzt wird gezockt!",
 };
 
 export default function RootLayout({
